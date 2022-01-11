7643c358eeef8338d402645e1b321c35a1ae7ba7
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-01-11 00:34:20 -0500

    [ADD]Full list of scales

--- src/utils/scales.js
+++ src/utils/scales.js
@@ -1,2 +1,20 @@
-export const nameTitles = { 'Categories': 'Categorías', 'Scales': 'Escalas' };
-export const nameScales = ['Mini Nutricional', 'Yessaveh'];
+export const nameTitles = { 'Categories': 'Categorías', 'Scales': 'Lista de Escalas' };
+export const nameScales = [
+   'Nutritional Assessment Short Form',
+   'Índice de Barthel',
+   'Escala de Lawton y Brody',
+   'Yessavage',
+   'Escala Gijon',
+   'Escala Braden',
+   'Cuestionario de Edimburgo',
+   'SCORE',
+   'Escala Framingham',
+   'CAM',
+   'Escala Has-bled',
+   'Escala Padua',
+   'Escala Caprini',
+   'Índice de Comorbilidades de Charlson',
+   'Mini-Mental State Examination',
+   'Cuestionario CAGE',
+   'Escala Morse',
+];
