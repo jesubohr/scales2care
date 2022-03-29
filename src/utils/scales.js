9880a54cbfb1c9ef3ff0827233109042e00f6df6
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-03-28 22:02:10 -0500

    Changed format of Questions

--- src/utils/scales.js
+++ src/utils/scales.js
@@ -19,64 +19,61 @@ export const nameScales = [
 ];
 
 export const scalesQuestions = [
-    {
-        scale: 'Nutritional Assessment Short Form',
-        questions: [
-            {
-                question: '¿Ha comido menos por falta de apetito o algún otro problema en los últimos tres meses?',
-                answers: [
-                    {
-                        answer: 'Ha comido mucho menos',
-                        value: 0,
-                    },
-                    {
-                        answer: 'Ha comido menos',
-                        value: 1,
-                    },
-                    {
-                        answer: 'Ha comido igual',
-                        value: 2,
-                    },
-                ],
-            },
-            {
-                question: 'Pérdida reciente de peso (< 3 meses)',
-                answers: [
-                    {
-                        answer: 'Pérdida > 3kg',
-                        value: 0,
-                    },
-                    {
-                        answer: 'Pérdida entre 1 - 3kg',
-                        value: 1,
-                    },
-                    {
-                        answer: 'No sabe',
-                        value: 2,
-                    },
-                    {
-                        answer: 'No ha habido pérdida',
-                        value: 3,
-                    },
-                ],
-            },
-            {
-                question: '¿Ha comido menos por falta de apetito o algún otro problema en los últimos tres meses?',
-                answers: [
-                    {
-                        answer: 'Ha comido mucho menos',
-                        value: 0,
-                    },
-                    {
-                        answer: 'Ha comido menos',
-                        value: 1,
-                    },
-                    {
-                        answer: 'Ha comido igual',
-                        value: 2,
-                    },
-                ],
-            }
-        ]
-    }
+    [
+        {
+            question: '¿Ha comido menos por falta de apetito o algún otro problema en los últimos tres meses?',
+            answers: [
+                {
+                    answer: 'Ha comido mucho menos',
+                    value: 0,
+                },
+                {
+                    answer: 'Ha comido menos',
+                    value: 1,
+                },
+                {
+                    answer: 'Ha comido igual',
+                    value: 2,
+                },
+            ],
+        },
+        {
+            question: 'Pérdida reciente de peso (< 3 meses)',
+            answers: [
+                {
+                    answer: 'Pérdida > 3kg',
+                    value: 0,
+                },
+                {
+                    answer: 'Pérdida entre 1 - 3kg',
+                    value: 1,
+                },
+                {
+                    answer: 'No sabe',
+                    value: 2,
+                },
+                {
+                    answer: 'No ha habido pérdida',
+                    value: 3,
+                },
+            ],
+        },
+        {
+            question: '¿Ha comido menos por falta de apetito o algún otro problema en los últimos tres meses?',
+            answers: [
+                {
+                    answer: 'Ha comido mucho menos',
+                    value: 0,
+                },
+                {
+                    answer: 'Ha comido menos',
+                    value: 1,
+                },
+                {
+                    answer: 'Ha comido igual',
+                    value: 2,
+                },
+            ],
+        }
+    ]
 ]
--- src/views/Scale.jsx
+++ src/views/Scale.jsx
@@ -7,14 +7,14 @@ function Scale () {
     const { scaleId } = useParams();
     const scaleIndex = Number(scaleId) - 1;
     const scaleName = nameScales[scaleIndex];
-    const scaleQuestions = scalesQuestions[scaleIndex];
+    const scaleQuestions = scalesQuestions[scaleIndex] ?? [];
     return (
         <MainContent className="py-10 px-5">
             <h2 className="mb-7 text-center text-gray-800 dark:text-gray-300 text-4xl font-bold">
                 { scaleName }
             </h2>
             <section className="flex flex-col gap-y-6 max-w-[34rem] w-11/12">
-                { scaleQuestions.questions.map(({ question, answers }, index) => {
+                { scaleQuestions.map(({ question, answers }, index) => {
                     return (
                         <ScaleQuestion
                             key={ index }
