48f3f8b270f4e281b0779a004104137c819095b9
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-22 23:14:07 -0500

    Minor fix on styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
       md: "700px",
       lg: "976px",
       gx: "1024px",
-      xl: "1440px",
+      xl: "1400px",
     },
     extend: {
       colors: {
         "med-blue": "#0F67FF",
         "med-sky": "#00A8FF",
         "med-green": "#007C63",
+        "dark-blue": "#001B2E"
       },
       fontFamily: {
         noto: ["Noto Sans", "ui-sans-serif"],
