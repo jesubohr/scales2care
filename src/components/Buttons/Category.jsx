531813ad0ca87f5691cfde1de96de9cfa23e086e
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-27 14:37:42 -0500

    Changed Category Titles

--- src/components/Buttons/Category.jsx
+++ src/components/Buttons/Category.jsx
@@ -9,9 +9,8 @@ export default function CategoryButton ({ title, color = false }) {
         <article
             className={
                 "py-5 px-5 " +
-                "max-w-md max-h-28 " +
+                "max-w-md w-full max-h-28 " +
                 "rounded cursor-pointer " +
-                "sm:w-[46%] " +
                 "dark:text-gray-50 " +
                 "backdrop-blur-sm backdrop-brightness-50 backdrop-opacity-10 dark:backdrop-brightness-200 dark:backdrop-opacity-60 " +
                 `border-4 ${color ? "border-med-sky" : "border-med-blue"}`
@@ -19,7 +18,7 @@ export default function CategoryButton ({ title, color = false }) {
             onClick={ () => { navigate('/app/categories/' + urlTitle); } }
         >
             <Icon icon="beat" viewBox="31" size="36" className={`absolute -top-4 -left-4 p-0.5 ${color ? "bg-med-sky" : "bg-med-blue"} rounded-sm text-white`} />
-            <h3 className="max-w-fit text-2xl font-medium">{ title }</h3>
+            <h3 className="w-fit text-2xl font-medium">{ title }</h3>
         </article>
     );
 }
